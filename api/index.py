6bec1c89bee235621598b2300fd90be116991bc2
Jems Jayswal <23f3004103@ds.study.iitm.ac.in>
2026-01-11 20:43:31 +0545

    fix 2

--- api/index.py
+++ api/index.py
@@ -12,8 +12,8 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["POST", "OPTIONS", "GET"],
+    allow_credentials=False,
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
